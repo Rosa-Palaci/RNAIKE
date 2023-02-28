a04655a155c8104abf2fb02b2f203a638f07848b
Rosa Palaci <palacibelrose431@gmail.com>
2023-02-27 18:57:56 -0600

    Se cargo las imagenes de objetos y logo

---
+++ src/assets/cards.js
@@ -0,0 +1,53 @@
+import image1 from "./1.svg";
+import image2 from "./2.svg";
+import image3 from "./3.svg";
+import image4 from "./4.svg";
+import image5 from "./5.svg";
+import image6 from "./6.svg";
+import image7 from "./7.svg";
+import image8 from "./8.svg";
+import image9 from "./9.svg";
+import image10 from "./10.svg";
+
+export const cards = [
+  {
+    id: 1,
+    image: image1,
+  },
+  {
+    id: 2,
+    image: image2,
+  },
+  {
+    id: 3,
+    image: image3,
+  },
+  {
+    id: 4,
+    image: image4,
+  },
+  {
+    id: 5,
+    image: image5,
+  },
+  {
+    id: 6,
+    image: image6,
+  },
+  {
+    id: 7,
+    image: image7,
+  },
+  {
+    id: 8,
+    image: image8,
+  },
+  {
+    id: 9,
+    image: image9,
+  },
+  {
+    id: 10,
+    image: image10,
+  },
+];
