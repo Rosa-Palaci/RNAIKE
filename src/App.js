6fc6e0155b1e90ead0402a261656a4e0c8f8b827
Rosa Palaci <palacibelrose431@gmail.com>
2023-03-01 15:49:26 -0600

    Todo chido

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
-import Carusel from "./components/Carusel";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Carusel />,
+    element: <Landing />,
   },
   {
     path: "/login",
